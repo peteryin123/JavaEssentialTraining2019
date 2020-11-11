c96b08eea9e25bb2da7202e47203353b76360ad0
peteryin123 <peteryin123@gmail.com>
2020-11-11 12:43:36 -0800

    My answer to the challenge.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var number = scanner.nextDouble();
+
+        System.out.print("Enter a numeric value: ");
+        var number2 = scanner.nextDouble();
+
+        double divide = number / number2;
+        System.out.print("The answer is " + divide);
     }
 }
